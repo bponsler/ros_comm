51d544ee0ef2935f112f2369529fe058074f24f8
Brett Ponsler <ponsler@gmail.com>
2017-07-23 16:17:40 +0000

    Initial port of xmlrpcpp
    Initial port of the xmlrpcpp package to ROS 2

--- utilities/xmlrpcpp/include/xmlrpcpp/XmlRpcDecl.h
+++ utilities/xmlrpcpp/include/xmlrpcpp/XmlRpcDecl.h
@@ -40,16 +40,6 @@
 #ifndef XMLRPCPP_DECL_H_INCLUDED
 #define XMLRPCPP_DECL_H_INCLUDED
 
-#include <ros/macros.h>
-
-#ifdef ROS_BUILD_SHARED_LIBS // ros is being built around shared libraries
-  #ifdef xmlrpcpp_EXPORTS // we are building a shared lib/dll
-    #define XMLRPCPP_DECL ROS_HELPER_EXPORT
-  #else // we are using shared lib/dll
-    #define XMLRPCPP_DECL ROS_HELPER_IMPORT
-  #endif
-#else // ros is being built around static libraries
-  #define XMLRPCPP_DECL
-#endif
+#define XMLRPCPP_DECL
 
 #endif /* XMLRPCPP_DECL_H_INCLUDED */
